417212c92c8e4f0159f4312ef6f020f7b7291356
Rahiman1999 <81863558+Rahiman1999@users.noreply.github.com>
2024-03-19 07:49:17 +0530

    Time: 792 ms (48.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ problems/175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select p.firstName,p.lastName,A.city,A.state from Person p left join Address A using(personId);
+
+-- select p.firstName,p.lastName,A.city,A.state from Person p left join Address A on P.personId=A.personId;
