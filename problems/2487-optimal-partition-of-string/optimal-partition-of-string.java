6e82210de130474d45a07950f188329363addd09
Rahiman1999 <81863558+Rahiman1999@users.noreply.github.com>
2024-08-18 01:39:44 +0530

    Time: 38 ms (14.42%) | Memory: 45.3 MB (43.54%) - LeetSync

---
+++ problems/2487-optimal-partition-of-string/optimal-partition-of-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int partitionString(String s) {
+        int i=0,j=0,count=0;
+        HashSet<Character> hs = new HashSet<>();
+        while(i<s.length()&&j<s.length()){
+           if(!hs.contains(s.charAt(j))){
+              hs.add(s.charAt(j++));
+           }else{
+              count++;
+              while(hs.size()!=0){
+                hs.remove(s.charAt(i++));
+              }
+           }
+            
+        }
+        return count+1;
+        
+    }
+}
