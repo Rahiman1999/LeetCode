f43db601573d3c78887538e218c15c777ca21e53
Rahiman1999 <81863558+Rahiman1999@users.noreply.github.com>
2024-08-20 22:40:58 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (31.30%) - LeetSync

--- problems/231-power-of-two/power-of-two.java
+++ problems/231-power-of-two/power-of-two.java
@@ -1,22 +1,13 @@
-// class Solution {
-//     public boolean isPowerOfTwo(int n) {
-//         int k=1,i=1;
-//         while(k<n&&i<32){
-//            k=k<<1;
-//            i++;
-//         }
-//         if(k==n){
-//             return true;
-//         }
-//         return false;
-//     }
-// }
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        long m=n;
-        if(n==0){
-            return false;
+        int k=1,i=1;
+        while(k<n&&i<32){
+           k=k<<1;
+           i++;
         }
-        return ((m & m-1)==0);
+        if(k==n){
+            return true;
+        }
+        return false;
     }
 }
