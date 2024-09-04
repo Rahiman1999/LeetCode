dc51590b3fab74ce88139f02c22e65140384218a
Rahiman1999 <81863558+Rahiman1999@users.noreply.github.com>
2024-09-04 21:29:58 +0530

    Time: 1 ms (99.89%) | Memory: 43.4 MB (50.63%) - LeetSync

--- problems/90-subsets-ii/subsets-ii.java
+++ problems/90-subsets-ii/subsets-ii.java
@@ -1,29 +1,51 @@
+// class Solution {
+//     Set<List<Integer>> ans = new HashSet<>();
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         printSubset(nums);
+//         return new ArrayList<>(ans);
+//     }
+//     public void printSubset(int[] nums){
+//         int n = nums.length;
+//         int count = 0;
+//         for(int i=0;i<(1<<n);i++){
+//             printElement(nums,i,count);
+//             count++;
+//         }
+//     }
+//     public void printElement(int[] nums,int n,int index){
+//         List<Integer> ans1 = new ArrayList<>();
+//         int j=0;
+//         while(n>0){
+//             int last_bit = (n&1);
+//             if(last_bit==1){
+//                 ans1.add(nums[j]);
+//             }
+//             j++;
+//             n=n>>1;
+//         }
+//         Collections.sort(ans1);
+//         ans.add(ans1);
+//     }
+// }
 class Solution {
-    Set<List<Integer>> ans = new HashSet<>();
+    List<List<Integer>> result = new ArrayList<List<Integer>>();
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        printSubset(nums);
-        return new ArrayList<>(ans);
+        Arrays.sort(nums);
+        subset(nums, 0, new ArrayList());
+        return result;
     }
-    public void printSubset(int[] nums){
-        int n = nums.length;
-        int count = 0;
-        for(int i=0;i<(1<<n);i++){
-            printElement(nums,i,count);
-            count++;
-        }
-    }
-    public void printElement(int[] nums,int n,int index){
-        List<Integer> ans1 = new ArrayList<>();
-        int j=0;
-        while(n>0){
-            int last_bit = (n&1);
-            if(last_bit==1){
-                ans1.add(nums[j]);
+
+    public void subset(int[] nums, int start, List<Integer> runningResult) {
+        
+        result.add(new ArrayList(runningResult));
+
+        for(int i=start; i<nums.length; i++) {
+            if(i != start && nums[i] == nums[i-1]){
+                continue;
             }
-            j++;
-            n=n>>1;
+            runningResult.add(nums[i]);
+            subset(nums, i+1, runningResult);
+            runningResult.remove(runningResult.size()-1);
         }
-        Collections.sort(ans1);
-        ans.add(ans1);
     }
 }
