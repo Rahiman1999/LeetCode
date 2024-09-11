59e333b5b0dba8c502f4e3a2d6754d1b05548a24
Rahiman1999 <81863558+Rahiman1999@users.noreply.github.com>
2024-09-12 02:50:06 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (65.35%) - LeetSync

--- problems/216-combination-sum-iii/combination-sum-iii.java
+++ problems/216-combination-sum-iii/combination-sum-iii.java
@@ -2,26 +2,24 @@ class Solution {
     Set<List<Integer>> hs = new HashSet<>();
     List<Integer> ans = new ArrayList<>();
     public List<List<Integer>> combinationSum3(int k, int n) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        solve(arr,k,n,0);
+        solve(k,n,1);
         return new ArrayList<>(hs);
     }
-    public void solve(int[] arr,int k,int n,int i){
-        if(ans.size()==k){
-            if(n==0){
-                hs.add(new ArrayList<>(ans));
-            }
-            return;
-        }
-        if(i==arr.length){
+    public void solve(int k,int n,int i){
+        if(n==0&&k==0){
+            hs.add(new ArrayList<>(ans));
+            
             return;
         }
 
-        if(arr[i]<=n){
-            ans.add(arr[i]);
-            solve(arr,k,n-arr[i],i+1);
+        for(int j=i;j<=9;j++){
+            if(j>n){
+                break;
+            }
+            ans.add(j);
+            solve(k-1,n-j,j+1);
             ans.remove(ans.size()-1);
         }
-        solve(arr,k,n,i+1);
+        
     }
 }
