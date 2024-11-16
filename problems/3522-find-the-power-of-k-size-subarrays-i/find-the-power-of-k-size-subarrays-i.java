743d24e278dd99171e7d396b61d5ad77232aa051
Rahiman1999 <81863558+Rahiman1999@users.noreply.github.com>
2024-11-16 21:46:26 +0530

    Time: 2 ms (58.78%) | Memory: 45.3 MB (63.56%) - LeetSync

--- problems/3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
+++ problems/3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        HashSet<Integer> hs = new HashSet<>();
         int[] ans = new int[nums.length-k+1];
         Arrays.fill(ans,-1);
-        int max=0;
         for(int i=0;i<=nums.length-k;i++){
             boolean f= true;
             for(int j=i;j<i+k-1;j++){
